3c658c44c9ac56fd2b6782add1b7ffa3ab57b348
kantoku2 <sheema.sheemap@gmail.com>
2016-05-24 02:41:15 +0900

    fix typo

--- masalachai/trainers/vat.py
+++ masalachai/trainers/vat.py
@@ -34,11 +34,11 @@ class VirtualAdversarialTrainer(Trainer):
 
     def lds(self, x):
         def kl_divergence(p, q, eps=10e-8):
-            return F.sum(p * (F.log(p+eps) - F.log(q+eps))) / p.data.shape[0]
+            return chainer.functions.sum(p * (chainer.functions.log(p+eps) - chainer.functions.log(q+eps))) / p.data.shape[0]
 
         xp = cuda.cupy.get_array_module(x.data)
 
-        p = F.softmax(self.optimizer.target.predictor(x))
+        p = chainer.functions.softmax(self.optimizer.target.predictor(x))
         p.unchain_backward()
 
         # init d as a random unit vector
@@ -50,23 +50,23 @@ class VirtualAdversarialTrainer(Trainer):
         vr = chainer.Variable(r, volatile='off')
         for ip in six.moves.range(self.pitr):
             vd = chainer.Variable(d)
-            q = F.softmax(self.optimizer.target.predictor(x+vr+self.xi*d))
+            q = chainer.functions.softmax(self.optimizer.target.predictor(x+vr+self.xi*d))
             k = kl_divergence(p, q)
             k.backward()
             d = vr.grad / xp.sqrt(xp.sum(vr.grad*vr.grad, axis=1)).reshape(d.shape[0], 1)
         r_vadv = chainer.Variable(self.eps * d)
-        q_vadv = F.softmax(self.optimizer.target.predictor(x+r_vadv))
+        q_vadv = chainer.functions.softmax(self.optimizer.target.predictor(x+r_vadv))
         self.lds_loss = self.lam * kl_divergence(p, q_vadv)
         return self.lds_loss
 
-    def train(self, nitr, lbatchsize, ubatchsize, log_interval=100, test_batchsize=100, test_nitr=1):
+    def train(self, nitr, lbatchsize, ubatchsize, log_interval=100, test_interval=100, test_batchsize=100, test_nitr=1):
         # training
         self.train_batch = self.train_data.batch(lbatchsize, shuffle=True)
         self.unlabeled_batch = self.unlabeled_data.batch(ubatchsize, shuffle=True)
         supervised_loss = 0.
         unsupervised_loss = 0.
         train_acc = 0.
-        for i in six.moves.range(nitr):
+        for i in six.moves.range(1, nitr+1):
             supervised_loss += float(self.supervised_update(lbatchsize))
             self.optimizer.target.loss.unchain_backward()
             train_acc += float(self.optimizer.target.accuracy.data)
