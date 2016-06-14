287c0e56e223215a0fc9d3ff581faf6f7215d9d9
kantoku2 <sheema.sheemap@gmail.com>
2016-06-14 15:17:46 +0900

    add autoencoder model

---
+++ masalachai/models/autoencoder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from chainer import link
+from chainer.functions import mean_squared_error
+
+## Autoencoder Wrapper
+class AutoencodeModel(link.Chain):
+    def __init__(self, predictor, lossfun=mean_squared_error):
+        super(SiameseModel, self).__init__(predictor=predictor)
+        self.lossfun = lossfun
+        self.h = None
+        self.y = None
+        self.loss = None
+
+    def __call__(self, x):
+        self.y = None
+        self.loss = None
+        self.y = self.predictor(x)
+        self.loss = self.lossfun(self.y)
+        return self.loss
+
+    def predict(self, x):
+        self.h = self.predictor.encode(x)
+        return self.h
+
