8cb5965883495ebe46fb50e4203ff238144249ee
kantoku2 <sheema.sheemap@gmail.com>
2016-06-14 16:43:26 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = [ pandas_version, six_version, chainer_version ]
 
 setup(
         name = 'masalachai',
-        version = '0.3.5',
+        version = '0.3.6',
         packages = ['masalachai',
                     'masalachai.datafeeders',
                     'masalachai.trainers',
