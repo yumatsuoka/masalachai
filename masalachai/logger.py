d3583697a2c96ba8cdba233f4d37401fbfc03ce0
kantoku2 <sheema.sheemap@gmail.com>
2016-06-22 17:46:12 +0900

    edit logger

--- masalachai/logger.py
+++ masalachai/logger.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 
+import os
 import logging
-from cheryl import CherylAPI
+import threading
 
-class Logger(object):
+class Logger(threading.Thread):
     formatter = logging.Formatter(fmt='%(asctime)s %(name)8s %(levelname)8s: %(message)s',datefmt='%Y/%m/%d %p %I:%M:%S,',)
-    handler = logging.StreamHandler()
 
-    def __init__(self, name, level=logging.INFO, tofile=None, tocheryl=None, touser=None):
+    def __init__(self, name, level=logging.INFO, logfile=None):
+        super(Logger, self).__init__()
+
         # stream handler setting
+        self.handler = logging.StreamHandler()
         self.handler.setLevel(level)
         self.handler.setFormatter(self.formatter)
 
@@ -18,44 +21,44 @@ class Logger(object):
         self.logger.addHandler(self.handler)
 
         # file handler setting
-        if tofile is not None:
-            self.file_handler = logging.FileHandler(tofile)
+        if logfile is not None:
+            self.file_handler = logging.FileHandler(logfile)
             self.file_handler.setFormatter(self.formatter)
             self.logger.addHandler(self.file_handler)
-
-        # Cheryl API setting
-        self.cheryl = (tocheryl is not None) and (touser is not None)
-        if self.cheryl:
-            self.bot = CherylAPI(configfile=tocheryl)
-            self.user = touser
+        
+        self.mode = {'TRAIN': self.train_log, 'TEST': self.test_log, 'END': self.log_end}
+        self.queue = None
+        self.stop = None
 
 
     def __call__(self, msg):
         self.logger.info(msg)
-        if self.cheryl:
-            self.bot.post_direct_message_by(self.user, '['+os.uname()[1]+'] '+msg)
 
-    def log(self, itr, loss, acc=None, train=True):
-        if train and acc is not None:
-            log_str = '{0:d}, loss={1:.5f}, accuracy={2:.5f}'.format(itr, loss, acc)
-        elif train and acc is None:
-            log_str = '{0:d}, loss={1:.5f}'.format(itr, loss)
-        elif not train and acc is not None:
-            log_str = '[TEST], loss={0:.5f}, accuracy={1:.5f}'.format(loss, acc)
-        elif not train and acc is None:
-            log_str = '[TEST], loss={0:.5f}'.format(loss)
+
+    def setQueue(self, queue):
+        self.queue = queue
+
+
+    def run(self):
+        # queue check
+        assert self.queue is None, "Log Queue is None, use Logger.setQueue(queue) before calling me."
+
+        self.stop = threading.Event()
+        while not self.stop.is_set():
+            res = self.queue.get()
+            if res in self.mode:
+                log_func = self.mode[res]
+                continue
+            log_func(res)
+
+
+    def log_train(self, res):
+        log_str = '{0:d}, loss={1:.5f}, accuracy={2:.5f}'.format(res['iteration'], res['loss'], res['accuracy'])
         self.__call__(log_str)
 
-
-    def loss_acc_log(self, itr, loss, acc):
-        log_str = '{0:d}, loss={1:.5f}, accuracy={2:.5f}'.format(itr, loss, acc)
+    def log_test(self, res):
+        log_str = '[TEST], loss={0:.5f}, accuracy={1:.5f}'.format(res['loss'], res['accuracy'])
         self.__call__(log_str)
 
-    def loss_log(self, itr, loss):
-        log_str = '{0:d}, loss={1:.5f}'.format(itr, loss)
-        self.__call__(log_str)
-
-    def test_log(self,loss, acc):
-        log_str = '[TEST], loss={0:.5f}, accuracy={1:.5f}'.format(loss, acc)
-        self.__call__(log_str)
-
+    def log_end(self, *args):
+        self.stop.set()
