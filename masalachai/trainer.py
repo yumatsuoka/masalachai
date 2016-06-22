70deab2dd06979724342ab89eccded27a73c2061
kantoku2 <sheema.sheemap@gmail.com>
2016-06-22 16:08:20 +0900

    edit trainer

--- masalachai/trainer.py
+++ masalachai/trainer.py
@@ -1,50 +1,54 @@
 import numpy
 import six
 import chainer
+import queue
 from chainer import cuda
 
 from masalachai.logger import Logger
 from masalachai.datafeeder import DataFeeder
 
 class Trainer(object):
+    # モデルの学習を制御するクラス
     _preprocess_hooks = []
     _optimizer_param_schedulers = []
 
-    def __init__(self, optimizer, train_data, test_data, gpu, logging=True, logfile=None, logcheryl=None, loguser=None):
+    # 並列処理用の共有メモリ
+    _data_queue = queue.Queue()
+    _log_queue = queue.Queue()
+
+    def __init__(self, optimizer, train_data, logger, gpu=-1):
         self.optimizer = optimizer
         self.train_data = train_data
-        self.test_data = test_data
+        self.logger = logger
         self.gpu = gpu
-        self.logging = logging
-        if self.logging:
-            self.logger = Logger(__name__, tofile=logfile, tocheryl=logcheryl, touser=loguser)
 
-    def hook(self, func):
+        # 並列処理用のメモリを各モジュールに登録
+        self.train_data.setQueue(self._data_queue)
+        self.logger.setQueue(self._log_queue)
+
+
+    def hook_preprocess(self, func):
+        # データの前処理関数の登録
         self._preprocess_hooks.append(func)
 
     def add_optimizer_scheduler(self, s):
+        # Optimizer のパラメータスケジューラの登録
         self._optimizer_param_schedulers.append(s)
 
     def optimizer_param_process(self, t):
+        # Optimizer のパラメータスケジューラの駆動
         for s in self._optimizer_param_schedulers:
             self.optimizer.__dict__[s.param_name] = s.next(t)
 
+
     def predict(self, batchsize, train=False):
+        # 推論を実行
         raise NotImplementedError
 
     def train(self, nitr, batchsize):
+        # 学習を実行
         raise NotImplementedError
 
     def test(self, nitr, batchsize):
-        # testing
-        acc = 0.
-        loss = 0.
-        self.test_batch = self.test_data.batch(batchsize, shuffle=False)
-        for i in six.moves.range(nitr):
-            self.predict(batchsize)
-            acc += float(self.optimizer.target.accuracy.data) if self.optimizer.target.accuracy is not None else 0.
-            loss += self.optimizer.target.loss.data
-        # logging
-        if self.logging:
-            self.logger.test_log(float(loss/nitr), float(acc/nitr))
-        return loss/nitr, acc/nitr
+        # テストを実行
+        raise NotImplementedError
