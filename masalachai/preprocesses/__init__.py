2c82ac3fbd387c1d3ed2cbb4e380a2dff6189e7c
daiki_shimada_mba <sheema.sheemap@gmail.com>
2016-06-30 02:53:24 +0900

    update image preprocess

--- masalachai/preprocesses/__init__.py
+++ masalachai/preprocesses/__init__.py
@@ -1,5 +1,10 @@
 from masalachai.preprocesses import image_preprocess
 
+rotation = image_preprocess.rotation
+shift = image_preprocess.shift
+zoom = image_preprocess.zoom
+shear = image_preprocess.shear
+
 random_rotation = image_preprocess.random_rotation
 random_shift = image_preprocess.random_shift
 random_zoom = image_preprocess.random_zoom
--- masalachai/preprocesses/image_preprocess.py
+++ masalachai/preprocesses/image_preprocess.py
@@ -15,7 +15,11 @@ def transform_matrix_offset_center(matrix, x, y):
 
 
 
-def apply_transform(x, transform_matrix, channel_index=0, fill_mode='nearest', cval=0.):
+def apply_transform(x, 
+                    transform_matrix, 
+                    channel_index=0, 
+                    fill_mode='nearest', 
+                    cval=0.):
     x = numpy.rollaxis(x, channel_index, 0)
     final_affine_matrix = transform_matrix[:2, :2]
     final_offset = transform_matrix[:2, 2]
@@ -26,9 +30,14 @@ def apply_transform(x, transform_matrix, channel_index=0, fill_mode='nearest', c
     return x
 
 
-def random_rotation(x, rg, row_index=1, col_index=2, channel_index=0,
-                    fill_mode='nearest', cval=0.):
-    theta = numpy.pi / 180 * numpy.random.uniform(-rg, rg)
+def rotation(x, 
+             g, 
+             row_index=1, 
+             col_index=2, 
+             channel_index=0,
+             fill_mode='nearest', 
+             cval=0.):
+    theta = numpy.pi / 180 * g
     rotation_matrix = numpy.array([[numpy.cos(theta), -numpy.sin(theta), 0],
                                 [numpy.sin(theta), numpy.cos(theta), 0],
                                 [0, 0, 1]])
@@ -39,26 +48,60 @@ def random_rotation(x, rg, row_index=1, col_index=2, channel_index=0,
     return x
 
 
-def random_shift(x, wrg, hrg, row_index=1, col_index=2, channel_index=0,
-                 fill_mode='nearest', cval=0.):
+def random_rotation(x, 
+                    rg, 
+                    row_index=1, 
+                    col_index=2, 
+                    channel_index=0,
+                    fill_mode='nearest', 
+                    cval=0.):
+    g = numpy.random.uniform(-rg, rg)
+    return rotation(x, g, row_index=row_index, col_index=col_index, channel_index=channel_index, fill_mode=fill_mode, cval=cval)
+
+
+def shift(x, 
+          wg, 
+          hg, 
+          row_index=1, 
+          col_index=2, 
+          channel_index=0,
+          fill_mode='nearest', 
+          cval=0.):
     h, w = x.shape[row_index], x.shape[col_index]
-    tx = numpy.random.uniform(-hrg, hrg) * h
-    ty = numpy.random.uniform(-wrg, wrg) * w
+    tx = hg * h
+    ty = wg * w
     translation_matrix = numpy.array([[1, 0, tx],
-                                   [0, 1, ty],
-                                   [0, 0, 1]])
+                                      [0, 1, ty],
+                                      [0, 0, 1]])
 
     transform_matrix = translation_matrix  # no need to do offset
     x = apply_transform(x, transform_matrix, channel_index, fill_mode, cval)
     return x
 
 
-def random_shear(x, intensity, row_index=1, col_index=2, channel_index=0,
-                 fill_mode='nearest', cval=0.):
-    shear = numpy.random.uniform(-intensity, intensity)
-    shear_matrix = numpy.array([[1, -numpy.sin(shear), 0],
-                             [0, numpy.cos(shear), 0],
-                             [0, 0, 1]])
+def random_shift(x, 
+                 wrg, 
+                 hrg, 
+                 row_index=1, 
+                 col_index=2, 
+                 channel_index=0,
+                 fill_mode='nearest', 
+                 cval=0.):
+    wg = numpy.random.uniform(-hrg, hrg)
+    hg = numpy.random.uniform(-wrg, wrg)
+    return shift(x, wg, hg, row_index=row_index, col_index=col_index, channel_index=channel_index, fill_mode=fill_mode, cval=cval)  
+
+
+def shear(x, 
+          s, 
+          row_index=1, 
+          col_index=2, 
+          channel_index=0,
+          fill_mode='nearest', 
+          cval=0.):
+    shear_matrix = numpy.array([[1, -numpy.sin(s), 0],
+                                [0, numpy.cos(s), 0],
+                                [0, 0, 1]])
 
     h, w = x.shape[row_index], x.shape[col_index]
     transform_matrix = transform_matrix_offset_center(shear_matrix, h, w)
@@ -66,19 +109,33 @@ def random_shear(x, intensity, row_index=1, col_index=2, channel_index=0,
     return x
 
 
-def random_zoom(x, zoom_range, row_index=1, col_index=2, channel_index=0,
-                fill_mode='nearest', cval=0.):
-    if len(zoom_range) != 2:
-        raise Exception('zoom_range should be a tuple or list of two floats. '
-                        'Received arg: ', zoom_range)
+def random_shear(x, 
+                 intensity, 
+                 row_index=1, 
+                 col_index=2, 
+                 channel_index=0,
+                 fill_mode='nearest', 
+                 cval=0.):
+    s = numpy.random.uniform(-intensity, intensity)
+    return shear(x, s, row_index=row_index, col_index=col_index, channel_index=channel_index, fill_mode=fill_mode, cval=cval)  
 
-    if zoom_range[0] == 1 and zoom_range[1] == 1:
-        zx, zy = 1, 1
-    else:
-        zx, zy = numpy.random.uniform(zoom_range[0], zoom_range[1], 2)
+
+def zoom(x, 
+         zoom_rate, 
+         row_index=1, 
+         col_index=2, 
+         channel_index=0,
+         fill_mode='nearest', 
+         cval=0.):
+    if len(zoom_rate) != 2:
+        raise Exception('zoom_rate should be a tuple or list of two floats. '
+                        'Received arg: ', zoom_rate)
+
+    zx = zoom_rate[0]
+    zy = zoom_rate[1]
     zoom_matrix = numpy.array([[zx, 0, 0],
-                            [0, zy, 0],
-                            [0, 0, 1]])
+                               [0, zy, 0],
+                               [0, 0, 1]])
 
     h, w = x.shape[row_index], x.shape[col_index]
     transform_matrix = transform_matrix_offset_center(zoom_matrix, h, w)
@@ -86,7 +143,26 @@ def random_zoom(x, zoom_range, row_index=1, col_index=2, channel_index=0,
     return x
 
 
-def load_image(path, grayscale=False, target_size=None):
+def random_zoom(x, 
+                zoom_range, 
+                row_index=1, 
+                col_index=2, 
+                channel_index=0,
+                fill_mode='nearest', 
+                cval=0.):
+    if len(zoom_range) != 2:
+        raise Exception('zoom_range should be a tuple or list of two floats. '
+                        'Received arg: ', zoom_range)
+    if zoom_range[0] == 1 and zoom_range[1] == 1:
+        zx, zy = 1, 1
+    else:
+        zx, zy = numpy.random.uniform(zoom_range[0], zoom_range[1], 2)
+    return zoom(x, (zx, zy), row_index=row_index, col_index=col_index, channel_index=channel_index, fill_mode=fill_mode, cval=cval)  
+
+
+def load_image(path, 
+               grayscale=False, 
+               target_size=None):
     from PIL import Image
     img = Image.open(path)
     if grayscale:
