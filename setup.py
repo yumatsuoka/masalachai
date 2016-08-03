28efa2aa00e65fdb28c83d339d1bcd0449005cd4
kantoku2 <sheema.sheemap@gmail.com>
2016-08-03 17:07:00 +0900

    add install requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 pandas_version = 'pandas'
 six_version = 'six>=1.10.0'
 chainer_version = 'chainer>=1.12.0'
-install_requires = [ pandas_version, six_version, chainer_version ]
+pythonjsonlogger_version = 'python-json-logger'
+install_requires = [ pandas_version, six_version, chainer_version, pythonjsonlogger_version ]
 
 setup(
         name = 'masalachai',
