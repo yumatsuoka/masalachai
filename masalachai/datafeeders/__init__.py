8b871540b8424787430145c5adeff42a46685b20
Yuma Matsuoka <a.iebako.yuma@gmail.com>
2016-08-11 17:34:59 +0900

    implemented Triplet Net, so add triplet_datafeeder.py, triplet_model.py and example/mnist_triplet.py

--- masalachai/datafeeders/__init__.py
+++ masalachai/datafeeders/__init__.py
@@ -1,3 +1,5 @@
 from masalachai.datafeeders import siamese_datafeeder
+from masalachai.datafeeders import triplet_datafeeder
 
 SiameseFeeder = siamese_datafeeder.SiameseFeeder
+TripletFeeder = triplet_datafeeder.TripletFeeder
