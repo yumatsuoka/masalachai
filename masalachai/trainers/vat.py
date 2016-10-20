62e5b0d291a711ecf93ece38512a27441383b1eb
kantoku2 <sheema.sheemap@gmail.com>
2016-10-21 03:31:43 +0900

    fix vat code for semi-supervised metric learning

--- masalachai/trainers/vat.py
+++ masalachai/trainers/vat.py
@@ -10,7 +10,25 @@ def _axis_tuple(ary):
     return tuple([i for i in six.moves.range(ary.ndim)])
 
 
+def kl_divergence(p, q, eps=10e-8):
+    return chainer.functions.sum(p * (
+        chainer.functions.log(p + eps) - chainer.functions.log(
+            q + eps))) / p.data.shape[0]
+
+def euclidean_d(p, q):
+    return chainer.functions.sum(chainer.functions.sqrt(
+        (p-q) * (p-q))) / p.data.shape[0]
+
+
 class VirtualAdversarialTrainer(SupervisedTrainer):
+    distance_calculators = {
+        'kl_d': {
+            'distance': kl_divergence,
+            'activation': chainer.functions.softmax},
+        'euclidean_d': {
+            'distance': euclidean_d,
+            'activation': chainer.functions.identity}
+        }
 
     def __init__(self, optimizer, logger, train_data_feeders,
                  test_data_feeder, gpu=-1, eps=4.0, xi=0.1, lam=1., pitr=1, norm='kl_d'):
@@ -45,26 +63,16 @@ class VirtualAdversarialTrainer(SupervisedTrainer):
         return res
 
     def lds(self, x):
-        def kl_divergence(p, q, eps=10e-8):
-            return chainer.functions.sum(p * (
-                chainer.functions.log(p + eps) - chainer.functions.log(
-                    q + eps))) / p.data.shape[0]
-
-        def euclidean_d(p, q):
-            return chainer.functions.sum(chainer.functions.sqrt((p-q) ** 2), axis=1)
-        
-        calc_d = euclidean_d if self.norm == 'euclidean_d' else kl_divergence
-
         xp = cuda.cupy if self.gpu >= 0 else numpy
 
         x0, = x
 
-        p = chainer.functions.softmax(self.optimizer.target.predictor(x0))
+        p = self.distance_calculators[self.norm]['activation'](
+                self.optimizer.target.predictor(x0))
         p.unchain_backward()
 
         # init d as a random unit vector
         d = xp.random.normal(size=x0.data.shape).astype(xp.float32)
-        # d = d / xp.sqrt(xp.sum(d*d, axis=d.shape[1:])).reshape(d.shape[0], 1)
         d = d / xp.sqrt(xp.sum(d * d, axis=_axis_tuple(d)[1:], keepdims=True))
 
         # approximate r_vadv by power iteration method
@@ -72,14 +80,14 @@ class VirtualAdversarialTrainer(SupervisedTrainer):
         vr = chainer.Variable(r, volatile='off')
         for ip in six.moves.range(self.pitr):
             vd = chainer.Variable(d)
-            q = chainer.functions.softmax(
+            q = self.distance_calculators[self.norm]['activation'](
                 self.optimizer.target.predictor(x0 + vr + self.xi * d))
-            k = calc_d(p, q)
+            k = self.distance_calculators[self.norm]['distance'](p, q)
             k.backward()
             d = vr.grad / xp.sqrt(xp.sum(
                 vr.grad * vr.grad, axis=_axis_tuple(d)[1:], keepdims=True))
         self.r_vadv = chainer.Variable(self.eps * d)
         q_vadv = chainer.functions.softmax(
             self.optimizer.target.predictor(x0 + self.r_vadv))
-        self.lds_loss = self.lam * calc_d(p, q_vadv)
+        self.lds_loss = self.lam * self.distance_calculators[self.norm]['distance'](p, q_vadv)
         return self.lds_loss
