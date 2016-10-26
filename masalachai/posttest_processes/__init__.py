73acdf6fda9caa9a0682c7dcc6b88156daf4685e
kantoku2 <sheema.sheemap@gmail.com>
2016-10-27 02:14:34 +0900

    add EarlyStopping

--- masalachai/posttest_processes/__init__.py
+++ masalachai/posttest_processes/__init__.py
@@ -1,3 +1,5 @@
 from masalachai.posttest_processes import snapshot_best
+from masalachai.posttest_processes import early_stopping
 
 SnapshotBest = snapshot_best.SnapshotBest
+EarlyStopping = early_stopping.EarlyStopping
--- masalachai/posttest_processes/snapshot_best.py
+++ masalachai/posttest_processes/snapshot_best.py
@@ -3,7 +3,6 @@
 import os
 from masalachai.posttest_process import PostTestProcess
 from chainer import serializers
-from scipy.interpolate import InterpolatedUnivariateSpline
 
 class SnapshotBest(PostTestProcess):
 
@@ -15,16 +14,19 @@ class SnapshotBest(PostTestProcess):
         self.iteration = 0
         self.loss = None
 
+    def save_model(self, test_res):
+        # Adding contents of test_res to instance variables, 
+        # we can use them in filename format.
+        self.__dict__.update({k:v for k,v in test_res.items() if k not in self.__dict__})
+        filename = self.filename.format(self.__dict__)
+        serializers.save_npz(filename, self.model)
+
     def __call__(self, test_res):
         value = test_res[self.observed]
         self.iteration = test_res['iteration']
 
-        # Adding contents of test_res to instance variables, 
-        # we can use them in filename format.
-        self.__dict__.update({k:v for k,v in test_res.items() if k not in self.__dict__})
-        if self.loss is None or value < self.loss:
-            self.loss = value
-            if self.iteration > self.patience:
-                filename = self.filename.format(self.__dict__)
-                serializers.save_npz(filename, self.model)
+        if self.iteration > self.patience:
+            if self.loss is None or value < self.loss:
+                self.loss = value
+                self.save_model(test_res)
         return False
