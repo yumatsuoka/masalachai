eb1c67249238b01657e7efed3b1bf715da839021
kantoku2 <sheema.sheemap@gmail.com>
2016-10-27 03:05:19 +0900

    add only_best option

--- masalachai/posttest_processes/snapshot_best.py
+++ masalachai/posttest_processes/snapshot_best.py
@@ -6,20 +6,32 @@ from chainer import serializers
 
 class SnapshotBest(PostTestProcess):
 
-    def __init__(self, model, filename, observed='loss', patience=1):
+    def __init__(self, model, filename, observed='loss', patience=1, best_only=False):
         self.model = model
         self.filename = filename
         self.observed = observed
         self.patience = patience
         self.iteration = 0
         self.loss = None
+        self.best_only = best_only
+        self.previous_file = None
+
 
     def save_model(self, test_res):
         # Adding contents of test_res to instance variables, 
         # we can use them in filename format.
         self.__dict__.update({k:v for k,v in test_res.items() if k not in self.__dict__})
         filename = self.filename.format(**self.__dict__)
+
+        # dump serialized model
         serializers.save_npz(filename, self.model)
+
+        # remove old file
+        if self.best_only:
+            if self.previous_file is not None:
+                os.remove(self.previous_file)
+            self.previous_file = filename
+
 
     def __call__(self, test_res):
         value = test_res[self.observed]
